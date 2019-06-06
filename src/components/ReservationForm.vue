2ec3d3be2d57750d7646adf98cbb84d743869b96
Maciek <mfrankowski@fortech.krakow.pl>
2019-06-06 13:55:11 +0200

    Homepage with simple form created.

---
+++ src/components/ReservationForm.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="ReservationForm p-5 bg-white w-1/2">
+        <input-select :label="'Pickup location'" :options="[
+            {
+                label: 'New York',
+                value: 1
+            },
+            {
+                label: 'Paris',
+                value: 2
+            },
+            {
+                label: 'Warsaw',
+                value: 3
+            }
+        ]"/>
+    </div>
+</template>
+
+<script>
+import InputSelect from '@/components/forms/InputSelect';
+
+export default {
+    components: {
+        InputSelect
+    }    
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="home">
-      <vehicle-list/>
+     <div class="HeroBlock bg-gray-500 h-screen">
+         <div class="container mx-auto flex justify-center items-center h-screen">
+             <reservation-form/>
+         </div>
+     </div>
   </div>
 </template>
 
@@ -8,12 +12,14 @@
 // @ is an alias to /src
 import CustomButton from '@/components/Button'
 import VehicleList from '@/components/VehicleList'
+import ReservationForm from '@/components/ReservationForm'
 
 export default {
   name: 'home',
   components: {
       CustomButton,
-      VehicleList
+      VehicleList,
+      ReservationForm
   }
 }
 </script>
